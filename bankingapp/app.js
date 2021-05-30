acb8f7523cccff38110e959fbba737e217a7fc75
Mico Ongkeko <micoongkeko@gmail.com>
2021-05-31 01:56:21 +0800

    Initial Banking App Complete with Error Handlers

--- bankingapp/app.js
+++ bankingapp/app.js
@@ -1,21 +1,30 @@
 /////////////////////   DOM  /////////////////////
-
 // Dashboard Page
 let currentBalance = document.getElementById('currentBalance');
 let expenseBtn = document.getElementById('expenseBtn');
-let depositBtn = document.getElementById('depositBtn');
+let depositBts = document.getElementById('depositBts');
 let withdrawBtn = document.getElementById('withdrawBtn');
 let transferBtn = document.getElementById('transferBtn');
 let sideMenuBtn = document.querySelector('#menu_logo');
 let sideMenuModal = document.querySelector('.sideMenuModal');
 let addExpenseModal = document.querySelector('.addExpenseModal');
 let depositModal = document.querySelector('.depositModal');
+let depositAmountInput;
 let withdrawModal = document.querySelector('.withdrawModal');
+let withdrawAmountInput;
 let transferModal = document.querySelector('.transferModal');
+let recipientAmountInput;
+let depositForm = document.getElementById('depositForm');
+let withdrawForm = document.getElementById('withdrawForm');
+let transferForm = document.getElementById('transferForm');
+let recipientUserKey;
+let recipientUser;
 
 // Create New Account Page
 let displayAllUsersBtn = document.querySelector('#displayAllUsersBtn');
 let displayAllUsersModal = document.querySelector('.displayAllUsersModal');
+let newAccForm = document.getElementById('newAccForm');
+let userStorage = [];
 
 // Side Menu Page
 let accNumEnterBtn = document.getElementById('accNumEnter');
@@ -43,7 +52,7 @@ accNumEnterBtn.addEventListener('click', function() {
 /////////////////////   MODALS  /////////////////////
 ////////// SIDE MENU MODAL
 sideMenuBtn.addEventListener('click',function(){
-    sideMenuModal.classList.add('openSideMenuModal') // Opens deposit modal on click of deposit button
+    sideMenuModal.classList.add('openSideMenuModal') // Opens side menu modal on click of deposit button
 })
 createNewAccSideMenu.addEventListener('click', function() {
     window.open(newuser.html); // Opens newuser.html when clicked from sidemenumodal
@@ -52,109 +61,118 @@ createNewAccSideMenu.addEventListener('click', function() {
 // let addExpenseItemInput = document.querySelector('#addExpenseItemInput').value;
 if(addExpenseModal) {
     expenseBtn.addEventListener('click',function(){
-    addExpenseModal.classList.add('openAddExpenseModal') // Opens deposit modal on click of deposit button
-})};
-
+    if(activeUser == undefined) { // Checks activeUser
+        alert("Please enter a valid account number before trying a transaction!")
+    } else {
+        addExpenseModal.classList.add('openAddExpenseModal') // Opens deposit modal on click of deposit button
+        }      
+    })
+};
 ////////// DEPOSIT MODAL
 if(depositModal) { // Checks for existence of depositModal (true in index.html, false in newuser.html)
-    depositBtn.addEventListener('click',function(){
+    depositBts.addEventListener('click',function(){
         if(activeUser == undefined) { // Checks activeUser
             alert("Please enter a valid account number before trying a transaction!")
-        } else 
+        } else {
             depositModal.classList.add('openDepositModal') // Opens deposit modal on click of deposit button
-            let depositForm = document.getElementById('depositForm');
+        }
+    })
+}
 
-            depositForm.addEventListener('submit', function(e){
-                let depositAmountInput = document.getElementById('depositAmountInput').value;
-                console.log("activeUser.balance: " + activeUser.balance + "depositAmountInput:" + depositAmountInput);
-                activeUser.balance = parseInt(activeUser.balance) + parseInt(depositAmountInput);
-                console.log("activeUser.balance: " + activeUser.balance + "depositAmountInput:" + depositAmountInput);
-                currentBalance.innerHTML = "PHP " + activeUser.balance.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-                console.log(activeUser)
-                localStorage.setItem(activeUserKey, JSON.stringify(activeUser));
-                alert(`xxxSuccessfuly deposited PHP ${depositAmountInput}`);
-                // depositForm.reset();
-                depositModal.classList.remove('openDepositModal');
-                e.preventDefault();
-                })
-})};
+if(depositForm) {
+    depositForm.addEventListener('submit', function(e){
+        depositAmountInput = document.getElementById('depositAmountInput').value;
+        activeUser.balance = parseInt(activeUser.balance) + parseInt(depositAmountInput);
+        currentBalance.innerHTML = "PHP " + activeUser.balance.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+        localStorage.setItem(activeUserKey, JSON.stringify(activeUser));
+        alert(`Successfuly deposited PHP ${depositAmountInput}`);
+        depositModal.classList.remove('openDepositModal');
+        depositForm.reset();
+        e.preventDefault();
+    });
+}
 
+function checkAccountBalance(activeUser) {
+    if (activeUser.balance < 0) {
+        alert("You do not have enough to make ")
+    }
+}
 ////////// WITHDRAW MODAL
 // Opens deposit modal on click of deposit button
 if(withdrawModal) {    
     withdrawBtn.addEventListener('click',function(){
         if(activeUser == undefined) { // Checks activeUser
             alert("Please enter a valid account number before trying a transaction!")
-        } else 
-            withdrawModal.classList.add('openWithdrawModal');
-            let withdrawModalBtn = document.getElementById('withdrawModalBtn');
-            
-            withdrawModalBtn.addEventListener('click', function(){
-            let withdrawAmountInput = document.getElementById('withdrawAmountInput').value;
-            activeUser.balance = parseInt(activeUser.balance) - parseInt(withdrawAmountInput);
-            let currentBalance = document.getElementById('currentBalance');
-            currentBalance.innerHTML = "PHP " + activeUser.balance.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-            console.log(activeUser)
-            localStorage.setItem(activeUserKey, JSON.stringify(activeUser));
-            alert(`Successfuly withdrew PHP ${withdrawAmountInput}`);
-            withdrawModal.classList.remove('openWithdrawModal');
+        } else {
+            withdrawModal.classList.add('openWithdrawModal');   
+        }
     })
-})};
+};
+
+if(withdrawForm) {
+    withdrawForm.addEventListener('submit', function(e){
+        withdrawAmountInput = document.getElementById('withdrawAmountInput').value;
+        activeUser.balance = parseInt(activeUser.balance) - parseInt(withdrawAmountInput);
+        currentBalance = document.getElementById('currentBalance');
+        currentBalance.innerHTML = "PHP " + activeUser.balance.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+        localStorage.setItem(activeUserKey, JSON.stringify(activeUser));
+        alert(`Successfuly withdrew PHP ${withdrawAmountInput}`);
+        withdrawModal.classList.remove('openWithdrawModal');
+        withdrawForm.reset();
+        e.preventDefault();
+    });
+}
 
 ////////// TRANSFER MODAL
 // Opens transfer modal on click of transfer button
 if(transferModal) {
     transferBtn.addEventListener('click',function(){
-    transferModal.classList.add('openTransferModal')
-     // When TRANSFER is clicked within the deposit modal, current balance is changed of current
-    
-     //  user and balance of recipient is changed as well
-        let transferModalBtn = document.getElementById('transferModalBtn');
-        transferModalBtn.addEventListener('click', function(){
-            let activeUserKey = document.getElementById('smmAccNumInput').value;
-            let activeUser = JSON.parse(window.localStorage.getItem(activeUserKey));
-            // console.log(activeUser)
-            if(activeUser == null) {
-                alert("Please enter a valid account number before trying a transaction!")
-            }
-            {
-            let recipientUserKey = document.getElementById('recipientAccNumberInput').value;
-            let recipientUser = JSON.parse(window.localStorage.getItem(recipientUserKey));
+        if(activeUser == undefined) {
+            alert("Please enter a valid account number before trying a transaction!")
+        } else {
+            transferModal.classList.add('openTransferModal')
+        }
+    })
+};
 
-            let recipientAmountInput = document.getElementById('recipientAmountInput').value;
+if(transferForm) {
+    transferForm.addEventListener('submit', function(e){
+        recipientUserKey = document.getElementById('recipientAccNumberInput').value;
+        recipientUser = JSON.parse(window.localStorage.getItem(recipientUserKey));
+        recipientAmountInput = document.getElementById('recipientAmountInput').value;
+        if (recipientUser && (recipientUserKey != activeUserKey)) {
             activeUser.balance = parseInt(activeUser.balance) - parseInt(recipientAmountInput);
             recipientUser.balance = parseInt(recipientUser.balance) + parseInt(recipientAmountInput);
-            let currentBalance = document.getElementById('currentBalance');
             currentBalance.innerHTML = "PHP " + activeUser.balance.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-            console.log(activeUser)
-
             // UPDATE values of local storage items
             localStorage.setItem(activeUserKey, JSON.stringify(activeUser));
             localStorage.setItem(recipientUserKey, JSON.stringify(recipientUser));
             alert(`Successfuly transferred PHP ${recipientAmountInput} to ${recipientUser.name}`);
             transferModal.classList.remove('openTransferModal');
-        }})
-})};
+            transferForm.reset();
+            e.preventDefault();
+        } else {
+            alert("Please enter a valid recipient account number.")
+        }
+    })
+}
 
 ////////// DISPLAY ALL USERS MODAL
 // Opens users modal on click of display button
 if(displayAllUsersModal) {    
     displayAllUsersBtn.addEventListener('click',function(){
-    displayAllUsersModal.classList.add('opendisplayAllUsersModal')
-
-    for (var i = 0; i < localStorage.length; i++){
-
-        let userKey = localStorage.key(i);
-        let userfromstorage = JSON.parse(localStorage.getItem(userKey));
-        let userlist = document.createElement("p");
-
-        userlist.innerHTML = "#" + userKey + " - "  + userfromstorage.name;
-        // userfromstorage.balance.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-        let userlistdiv = document.querySelector('.userlistdiv');
-        userlistdiv.appendChild(userlist)
-        // console.log(userfromstorage.name);
-    }
-})};
+        displayAllUsersModal.classList.add('opendisplayAllUsersModal')
+        for (var i = 0; i < localStorage.length; i++){
+            let userKey = localStorage.key(i);
+            let userfromstorage = JSON.parse(localStorage.getItem(userKey));
+            let userlist = document.createElement("p");
+            userlist.innerHTML = "#" + userKey + " - "  + userfromstorage.name;
+            let userlistdiv = document.querySelector('.userlistdiv');
+            userlistdiv.appendChild(userlist)
+            // console.log(userfromstorage.name);
+        }
+    })
+};
 
 // Closes modals on click outside of modal
 window.addEventListener("click", function(event){
@@ -192,36 +210,46 @@ class User {
     }
 }
 
-
-
-
-let createNewAccBtn = document.getElementById('createNewAcc');
-
-let userStorage = [];
-
-if (createNewAccBtn) {
-createNewAccBtn.addEventListener('click', (e) => {
-    // e.preventDefault();
-
-    let nameInput = document.getElementById('nameInput').value;
-    let emailInput = document.getElementById('emailInput').value;
-    let passwordInput = document.getElementById('passwordInput').value;
-    let initialDeposit = parseInt(document.getElementById('initialDepositInput').value);
-    let accountNumber = Math.floor(Math.random()*1000)
-
-
-
-    if (nameInput && emailInput && passwordInput && initialDeposit){
-        const newUser = new User(nameInput, emailInput, passwordInput, initialDeposit, accountNumber);
-        alert(`${nameInput} has successfuly created an account with an initial deposit of ${initialDeposit}! Your account number is ${accountNumber}, please take note of this as you will need this to transact later on.`);
-        localStorage.setItem(`${accountNumber}`, JSON.stringify(newUser));
-        userStorage.push(newUser);
-        console.log(newUser);
+function addToUserStorage() {
+    for (var i = 0; i < localStorage.length; i++){
+        let userKey = localStorage.key(i);
+        let userKeyObject = JSON.parse(window.localStorage.getItem(userKey))
+        // console.log(userKey)
+        // console.log(JSON.parse(window.localStorage.getItem(userKey)))
+        if (userStorage.includes(userKeyObject.name)) {
+            // console.log("already exists")
+        } else {
+            userStorage.push(userKeyObject.name)
+            // const newUser = new User(nameInput, emailInput, passwordInput, initialDeposit, accountNumber);                    alert(`${nameInput} has successfuly created an account with an initial deposit of ${initialDeposit}! Your account number is ${accountNumber}, please take note of this as you will need this to transact later on.`);
+            // localStorage.setItem(`${accountNumber}`, JSON.stringify(newUser));
+            // userStorage.push(nameInput);
+            // console.log(newUser);
+        }
     }
-    console.log(userStorage);
-})};
+}
 
-
+if (newAccForm) {
+    newAccForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let nameInput = document.getElementById('nameInput').value;
+        let emailInput = document.getElementById('emailInput').value;
+        let passwordInput = document.getElementById('passwordInput').value;
+        let initialDeposit = parseInt(document.getElementById('initialDepositInput').value);
+        let accountNumber = Math.floor(Math.random()*1000)
+        addToUserStorage();
+        if (userStorage.includes(nameInput)) {
+            alert("An account with the same name has already been created. Please create a new account or log in to your existing one from the side menu.");
+            newAccForm.reset();
+        } else {
+            const newUser = new User(nameInput, emailInput, passwordInput, initialDeposit, accountNumber);
+            alert(`${nameInput} has successfuly created an account with an initial deposit of ${initialDeposit}! Your account number is ${accountNumber}, please take note of this as you will need this to transact later on.`);
+            localStorage.setItem(`${accountNumber}`, JSON.stringify(newUser));
+            userStorage.push(nameInput);
+            console.log(newUser);
+        }
+        console.log("Current existing users on the platform are: " + userStorage);
+    })
+};
 
 
 
