90f42197f17d11fc68168cf1a3e6a400481c8445
Mico Ongkeko <micoongkeko@gmail.com>
2021-06-28 17:10:13 +0800

    Between.rb uploaded

--- rubyactivities/between.rb
+++ rubyactivities/between.rb
@@ -1,19 +1,3 @@
-# def check(x)
-#     if x > 100 && x < 0 
-#         puts "Enter a number between 0 and 100"
-#         x = gets.chomp.to_i
-#     elseif x > 0 && x < 50
-#         puts "The number is between 0 and 50"
-#     elseif x > 50 && x < 100
-#         puts "The number is between 50 and 100"
-#     end
-# end
-
-# puts "Enter your number."
-# # x = gets.chomp.to_i
-# check(gets.chomp.to_i)
-
-
 puts "Enter your number."
 x = gets.chomp.to_i
 if x > 100 && x < 0 
