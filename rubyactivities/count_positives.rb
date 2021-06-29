ea459a96d665b5ffa6f9ffcfe61f0c846271e48c
Mico Ongkeko <micoongkeko@gmail.com>
2021-06-29 19:38:12 +0800

    Count positives initial commit.

---
+++ rubyactivities/count_positives.rb
@@ -0,0 +1,13 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
+positive = 0
+negative = 0
+
+arr.each do |num|
+    if num > 0
+        positive += num
+    elsif num < 0
+        negative += num
+    end
+end
+
+puts "[#{positive}, #{negative}]"
